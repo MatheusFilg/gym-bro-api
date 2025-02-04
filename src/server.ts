ff9d20ddb488c69b81f7f33a763fff1555f83591
matheusf <matheus_falmeida@hotmail.com>
2025-02-03 21:55:09 -0300

    ajuste de CORS

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,9 @@ import { errorHandler } from "./error-handler";
 const app = fastify()
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173'
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 })
 
 app.register(fastifySwagger, {
